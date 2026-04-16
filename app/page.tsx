191f14564a38d11d89e85d3fa48ebabbe3c24614
Sadraw <sadrathescientist@gmail.com>
2026-04-16 16:14:51 +0200

    Basic landing page with two links to papers and CV pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,32 @@
+// home landing page 
 export default function Home() {
   return (
-    <main style={{ padding: "2rem", fontFamily: "serif", boxSizing: "-moz-initial"}}>
-    
-      <h1>Ratata</h1>
+    <main
+    style={{
+      height: "100vh",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center", 
+      alignItems: "center",
+      textAlign: "center",
+      fontFamily: "serif",
+      background: "#188639",
+    }}
+    >
+      <h1 style={{fontSize: "2rem", letterSpacing: "1px"
+      }}>
+        Sadra Daneshmand
+      </h1>
 
-      <p>MA ENGLISH STUDIES</p>
-      <p>
-        This website contains my academic work and research papers.
+      <p style={{ marginTop: "1rem", fontSize: "1.2rem"
+      }}>
+        MA English Studies · Discourse · Media · Language
       </p>
 
-      <a href="/papers">View Papers</a>
-      
-      
-      
-      </main>
-  )
+      <div style={{fontSize: "1.5rem", marginTop: "2.5rem", display: "flex", gap: "1.5rem"}}>
+        <a href="/papers">Papers</a>
+        <a href="/cv">CV</a>
+      </div>
+    </main>
+  );
 }
