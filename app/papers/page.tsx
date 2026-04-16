6116c4dbe888cdf75840facf3486478fb213f5f5
Sadraw <sadrathescientist@gmail.com>
2026-04-16 16:35:45 +0200

    added a new file

--- app/papers/page.tsx
+++ app/papers/page.tsx
@@ -1,27 +1,29 @@
-const papers = [
-    {
-        title: "When the Network Goes Down",
-        slug: "network-goes-down",
+import {getAllPapers} from "@/lib/papers";
+import { styleText } from "util";
 
-    },
-];
+export default function PapersPage() {
+    const papers = getAllPapers();
 
-
-export default function Papers() {
     return (
-        <main style={{padding: "2rem", fontFamily: "serif", fontSize: 35}}>
-            <h1>Papers</h1>
-            <ul>
-                {papers.map((paper) => (
-                    <li key={paper.slug}>
-                        <a href={`/papers/$(paper.slug)`}>
+        <main
+        style={{
+            maxWidth: "750px",
+            margin: "0 auto", 
+            padding: "4rem 1.5rem", 
+            fontFamily: "serif",
+        }}
+        >
+            <h1 style={{ fontSize: "2rem"}}>Papers</h1>
+            <div style={{ marginTop: "2rem"}}>
+                {papers.map((paper: any) => (
+                    <div key={paper.slug} style={{ marginBottom: "1.5rem"}}>
+                        <a href={`/papers/${paper.slug}`} style={{fontSize: "1.5rem"}}>
                         {paper.title}
                         </a>
-                    </li>
+                        
+                    </div>
                 ))}
-            </ul>
-
-
+            </div>
         </main>
     )
 }
