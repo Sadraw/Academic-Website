93f15f8c131d2b32e4c249bd9465af18da56b6a2
Sadraw <sadrathescientist@gmail.com>
2026-04-20 16:40:29 +0200

    starting with the dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+
+
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -25,9 +29,18 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        
+        <ThemeProvider>
+          
+          {children}
+          
+          </ThemeProvider>
+          
+        </body>
     </html>
   );
 }
