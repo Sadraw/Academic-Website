aa4cfbdd80d8328cd51815278a0bcfa377d8234a
Sadraw <sadrathescientist@gmail.com>
2026-04-20 17:50:35 +0200

    adding the needed attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         
-        <ThemeProvider>
+        <ThemeProvider attribute= "class" defaultTheme="system" enableSystem>
           
           {children}
           
