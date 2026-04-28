8638165aa82870191ebe13fdff86e9aa778a8f17
Sadraw <sadrathescientist@gmail.com>
2026-04-28 19:08:45 +0200

    looks better but still flickers

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
 
 
         
-        <ThemeProvider attribute= "class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute= "class" defaultTheme="system" enableSystem> 
           
           <nav 
           className=
