b6d8abce667e640b1258d058715494e179f9772e
Sadraw <sadrathescientist@gmail.com>
2026-04-28 13:24:26 +0200

    added the skeleton

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,26 +1,14 @@
 // contact page 
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
 import Link from "next/link";
 import { ThemeToggle } from "../components/ThemeToggle";
 
-export default function ContactPage() { 
+export default function ContactPage() {
     
-    const dir = path.join(process.cwd(), "content/contact");
-    const files = fs.readdirSync(dir);
-    
-    const papers = files.map((file) => {
-    const slug = file.replace(".md", "");
-    const fileContent = fs.readFileSync(path.join(dir, file), "utf8");
-    const { data } = matter(fileContent);
-    
-    
-    
-    
-    
+ return (
     <main>
-
-        
+        <p>hello world!</p>
     </main>
+ )   
+    
+    
 }
