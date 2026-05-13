bea7ce6aee75350ccb25fe0d27921630d4fb6fc7
Sadraw <sadrathescientist@gmail.com>
2026-05-13 11:01:25 +0200

    added the new yotube video

--- app/media/videos/videos.data.tsx
+++ app/media/videos/videos.data.tsx
@@ -1,5 +1,17 @@
 export const videos = [
   {
+        id: "5e3FdrU7EJQ",
+    title: "Sarwā - Playing Keys on a Lazy Saturday Evening",
+    date: "2026",
+    description: "Late Saturday evening vibes 🌙 \n Just messing around on the MIDI keyboard while my friend chills in the background watching reels and laughing at random stuff 😂",
+  },
+  {
+  id: "ivfJG95Czss",
+  title: "Sarwā - Falling Into the Abyss",
+  date: "2025",
+  description: "First test upload",
+},
+  {
     id: "dQw4w9WgXcQ",
     title: "Rick Astley - Never Gonna Give you up",
     date: "2010",
@@ -10,11 +22,5 @@ export const videos = [
     title: "Immortal Technique - Dance With The Devil",
     date: "2008",
     description: "I once knew a guy named William",
-  },
-    {
-    id: "ivfJG95Czss",
-    title: "Sarwā - Falling Into the Abyss",
-    date: "2025",
-    description: "First test upload",
   },
 ];
