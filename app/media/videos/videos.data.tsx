020d7f737488e271f2d67d9761520657fffffaaf
Sadraw <sadrathescientist@gmail.com>
2026-05-20 12:13:09 +0200

    Add new videos

--- app/media/videos/videos.data.tsx
+++ app/media/videos/videos.data.tsx
@@ -1,22 +1,42 @@
 export const videos = [
+  
+    {
+      id: "iVq3P4-yYvc",
+      title: "Sarwā - Ghandoon ba Chai",
+      date: "2026",
+      description:
+        "Your Turkish Uncle Letting you Know you Need Some Persian Tea."
+    },
   {
-        id: "5e3FdrU7EJQ",
+    id: "IT-_4hVhe0k",
+    title: "Sarwā — I Will Play the Bass for you",
+    date: "2026",
+    description:
+      "Chilling With Friends on a Saturday Night."
+  },
+
+  {
+    id: "5e3FdrU7EJQ",
     title: "Sarwā - Playing Keys on a Lazy Saturday Evening",
     date: "2026",
-    description: "Late Saturday evening vibes 🌙 \n Just messing around on the MIDI keyboard while my friend chills in the background watching reels and laughing at random stuff 😂",
+    description:
+      "Late Saturday evening vibes 🌙 Just messing around on the MIDI keyboard while my friend chills in the background watching reels and laughing at random stuff 😂",
   },
+
   {
-  id: "ivfJG95Czss",
-  title: "Sarwā - Falling Into the Abyss",
-  date: "2025",
-  description: "First test upload",
-},
+    id: "ivfJG95Czss",
+    title: "Sarwā - Falling Into the Abyss",
+    date: "2025",
+    description: "First test upload",
+  },
+
   {
     id: "dQw4w9WgXcQ",
-    title: "Rick Astley - Never Gonna Give you up",
+    title: "Rick Astley - Never Gonna Give You Up",
     date: "2010",
     description: "First test upload",
   },
+
   {
     id: "qggxTtnKTMo",
     title: "Immortal Technique - Dance With The Devil",
